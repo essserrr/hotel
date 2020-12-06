535b4efd1491ab53c24373f4773830b7e563ffad
Essserrr <blizzz93@gmail.com>
2020-12-06 16:53:17 +0900

    Remove apply close handlers added

--- src/ui-kit/datepicker/datepicker.js
+++ src/ui-kit/datepicker/datepicker.js
@@ -1,56 +1,42 @@
 $(document).ready(function () {
     const dateNodes = $("[data-label-type=date]");
 
-    dateNodes.daterangepicker(
-        {
-            locale: {
-                cancelLabel: "Clear",
-                format: "dd/mm/yyyy",
-                separator: " - ",
-                applyLabel: "Применить",
-                cancelLabel: "Очистить",
-                fromLabel: "С",
-                toLabel: "До",
-                customRangeLabel: "Кастомный",
-                weekLabel: "Н",
-                daysOfWeek: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
-                monthNames: [
-                    "Январь",
-                    "Февраль",
-                    "Март",
-                    "Апрель",
-                    "Май",
-                    "Июнь",
-                    "Июль",
-                    "Август",
-                    "Сентябрь",
-                    "Октябрь",
-                    "Ноябрь",
-                    "Декабрь",
-                ],
-                firstDay: 1,
-            },
-
-            singleDatePicker: true,
-            linkedCalendars: false,
-            showCustomRangeLabel: false,
+    dateNodes.daterangepicker({
+        locale: {
+            cancelLabel: "Clear",
+            format: "dd/mm/yyyy",
+            separator: " - ",
+            applyLabel: "Применить",
+            cancelLabel: "Очистить",
+            fromLabel: "С",
+            toLabel: "До",
+            customRangeLabel: "Кастомный",
+            weekLabel: "Н",
+            daysOfWeek: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+            monthNames: [
+                "Январь",
+                "Февраль",
+                "Март",
+                "Апрель",
+                "Май",
+                "Июнь",
+                "Июль",
+                "Август",
+                "Сентябрь",
+                "Октябрь",
+                "Ноябрь",
+                "Декабрь",
+            ],
+            firstDay: 1,
         },
-        function (start, end, label) {
-            console.log(
-                "New date range selected: " +
-                    start.format("YYYY-MM-DD") +
-                    " to " +
-                    end.format("YYYY-MM-DD") +
-                    " (predefined range: " +
-                    label +
-                    ")"
-            );
-        }
-    );
+
+        singleDatePicker: true,
+        linkedCalendars: false,
+        showCustomRangeLabel: false,
+    });
 
     const formatMonth = function () {
         const activePicker = $("div.daterangepicker").filter(function () {
-            console.log(this.style.cssText.includes("display: block"));
             return this.style.cssText.includes("display: block");
         })[0];
 
@@ -61,6 +47,23 @@ $(document).ready(function () {
         month.html(monthAndYear[0] + "<br>" + monthAndYear[1]);
     };
 
+    const closeDropdown = function () {
+        $.uiDropdown.close();
+    };
+
+    const applyDate = function (ev, picker) {
+        $.uiDropdown.setValue(picker.startDate.format("DD.MM.YYYY"));
+    };
+
+    const removeDate = function () {
+        $.uiDropdown.setValue("");
+    };
+
+    dateNodes.on("apply.daterangepicker", applyDate);
+    dateNodes.on("cancel.daterangepicker", removeDate);
+
     dateNodes.on("show.daterangepicker", formatMonth);
+    dateNodes.on("hide.daterangepicker", closeDropdown);
+
     dateNodes.on("showCalendar.daterangepicker", formatMonth);
 });
