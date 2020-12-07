b3c2044f3a707cc2b443b655a56656b74d736650
Essserrr <blizzz93@gmail.com>
2020-12-07 18:46:15 +0900

    like button added

---
+++ src/ui-kit/buttons/like/like.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    const likeClickHandler = function () {
+        const target = $(this);
+        let value = target.val();
+        const checked = target.is(":checked");
+        const likeRoot = target.closest(".ui-like");
+
+        if (checked) {
+            value++;
+            target.val(value);
+            likeRoot.find(".ui-like__label").text(value);
+            likeRoot.toggleClass("ui-like--checked");
+        } else {
+            value--;
+            target.val(value);
+            likeRoot.find(".ui-like__label").text(value);
+            likeRoot.toggleClass("ui-like--checked");
+        }
+    };
+
+    $(".js-like").on("click", likeClickHandler);
+});
--- src/ui-kit/ui-kit.js
+++ src/ui-kit/ui-kit.js
@@ -1,3 +1,4 @@
 import "./dropdown/dropdown";
 import "./masks/date";
 import "./datepicker/datepicker";
+import "./buttons/like/like";
