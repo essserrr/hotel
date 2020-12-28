b538bfd9e40bb307cb16ec43f90ad9836dff605a
Essserrr <blizzz93@gmail.com>
2020-12-28 15:42:13 +0900

    dropdown now can be opened by default multiple times

--- src/ui-kit/dropdown/dropdown.js
+++ src/ui-kit/dropdown/dropdown.js
@@ -1,6 +1,8 @@
 $(function () {
     class Dropdown {
-        constructor() {
+        constructor({ canBeDefaultOpened }) {
+            this.defaultOpened = canBeDefaultOpened;
+
             this.getDropdownNodes = this.getDropdownNodes.bind(this);
             this.getCoordinates = this.getCoordinates.bind(this);
             this.setElementPosition = this.setElementPosition.bind(this);
@@ -14,73 +16,93 @@ $(function () {
             this.setValue = this.setValue.bind(this);
         }
 
-        getDropdownNodes() {
-            this.parent = $(this.nodeClicked).closest(".ui-dropdown");
-            this.label = this.parent.find(".js-dropdown__label");
-            this.input = this.parent.find(".js-dropdown__input");
-            this.container = this.parent.find(".js-dropdown__container");
-            this.element = this.parent.find(".ui-dropdown__element");
+        showDropHandler(event) {
+            this.show(event.target);
         }
 
-        getCoordinates() {
-            const width = $(this.label).outerWidth();
-            const height = $(this.label).outerHeight();
-            const borderBottom = parseInt(
-                $(this.label).css("border-left-width"),
-                10
-            );
-            let { left, top } = $(this.label).offset();
-            top = top - $(window).scrollTop() + height - borderBottom;
-            left = left - $(window).scrollLeft();
-            return { width, height, left, top, borderBottom };
+        show(nodeClicked) {
+            let nodes = this.getDropdownNodes(nodeClicked);
+            nodes.parent.attr("data-drop-opened", true);
+
+            this.setElementPosition(nodes);
+            nodes.container.show();
+
+            const resizeCallback = () => {
+                this.setElementPosition(nodes);
+            };
+
+            window.addEventListener("scroll", resizeCallback);
+            window.addEventListener("resize", resizeCallback);
+
+            const closeCallback = (event) => {
+                this.closeDropHandler(event, nodes, resizeCallback);
+            };
+            $(nodes.container).on("click", closeCallback);
         }
 
-        setElementPosition() {
+        getDropdownNodes(nodeClicked) {
+            let parent = $(nodeClicked).closest(".ui-dropdown");
+
+            return {
+                parent,
+                label: parent.find(".js-dropdown__label"),
+                input: parent.find(".js-dropdown__input"),
+                container: parent.find(".js-dropdown__container"),
+                element: parent.find(".ui-dropdown__element"),
+            };
+        }
+
+        setElementPosition(nodes) {
             //find label parameters
-            const coordinates = this.getCoordinates();
+            const coordinates = this.getCoordinates(nodes);
             //set current element style
-            this.element.css({
+            nodes.element.css({
                 width: coordinates.width,
                 top: coordinates.top,
                 left: coordinates.left,
             });
         }
 
-        show() {
-            this.getDropdownNodes();
-            this.parent.attr("data-drop-opened", true);
-            this.setElementPosition();
-            window.addEventListener("scroll", this.setElementPosition);
-            window.addEventListener("resize", this.setElementPosition);
-            this.container.show();
+        getCoordinates(nodes) {
+            const width = $(nodes.label).outerWidth();
+            const height = $(nodes.label).outerHeight();
+            const borderBottom = parseInt(
+                $(nodes.label).css("border-left-width"),
+                10
+            );
+            let { left, top } = $(nodes.label).offset();
+            top = top - $(window).scrollTop() + height - borderBottom;
+            left = left - $(window).scrollLeft();
+            return { width, height, left, top, borderBottom };
         }
 
-        close() {
-            this.container.hide();
-            this.parent.removeAttr("data-drop-opened");
-            $(window).off("scroll", this.setElementPosition);
-            $(window).off("resize", this.setElementPosition);
+        closeDropHandler(event, nodes, resizeCallback) {
+            if (!nodes.container.is(event.target)) return;
+            this.close(nodes, resizeCallback);
         }
 
-        showDropHandler(event) {
-            this.nodeClicked = event.target;
-            this.show();
-        }
-        closeDropHandler(event) {
-            if (!this.container.is(event.target)) return;
-            this.close();
+        close(nodes, resizeCallback) {
+            nodes.container.hide();
+            nodes.parent.removeAttr("data-drop-opened");
+            $(window).off("scroll", resizeCallback);
+            $(window).off("resize", resizeCallback);
         }
 
         setHandlers() {
             $(
                 ".js-dropdown__label, .js-dropdown__icon, .js-dropdown__input"
             ).on("click", this.showDropHandler);
-            $(".js-dropdown__container").on("click", this.closeDropHandler);
 
             $.uiDropdown = {
                 close: this.close,
                 setValue: this.setValue,
             };
+
+            if (this.defaultOpened) {
+                $(".js-dropdown__input[data-drop-default-opened]").trigger(
+                    "click"
+                );
+            }
         }
 
         setValue(value) {
@@ -88,6 +110,6 @@ $(function () {
         }
     }
 
-    let dropdownOpened = new Dropdown();
+    let dropdownOpened = new Dropdown({ canBeDefaultOpened: true });
     dropdownOpened.setHandlers();
 });
