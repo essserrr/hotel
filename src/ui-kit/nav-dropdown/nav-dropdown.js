42d611048093c455671d5734b89f58dfd9560e3d
Essserrr <blizzz93@gmail.com>
2020-12-30 14:06:56 +0900

    nav dropdown arrow added

--- src/ui-kit/nav-dropdown/nav-dropdown.js
+++ src/ui-kit/nav-dropdown/nav-dropdown.js
@@ -1,8 +1,13 @@
-$(".js-nav-dropdown").hover(
-    function () {
+$(function () {
+    const enterHandler = function () {
         $(".js-nav-dropdown__list", this).stop().slideDown(200);
-    },
-    function () {
+        $(this).attr("data-navdropdown-hover", true);
+    };
+    const leaveHandler = function () {
         $(".js-nav-dropdown__list", this).stop().slideUp(200);
-    }
-);
+        $(this).attr("data-navdropdown-hover", false);
+    };
+    const targetNode = ".js-nav-dropdown";
+    $(targetNode).on("mouseenter", enterHandler);
+    $(targetNode).on("mouseleave", leaveHandler);
+});
