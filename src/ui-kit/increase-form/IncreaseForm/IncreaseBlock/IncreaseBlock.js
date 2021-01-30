a0db19510085a7334b2f32084f158adcd9391b15
Essserrr <blizzz93@gmail.com>
2021-01-30 13:22:13 +0900

    view and model are private now

--- src/ui-kit/increase-form/IncreaseForm/IncreaseBlock/IncreaseBlock.js
+++ src/ui-kit/increase-form/IncreaseForm/IncreaseBlock/IncreaseBlock.js
@@ -5,14 +5,14 @@ const CONSTANTS = {
 
 class IncreaseBlock {
     constructor() {
-        this.view = {
+        this._view = {
             nodeClicked: null,
             buttonBlock: null,
             increaseButton: null,
             decreaseButton: null,
             valueLabel: null,
         };
-        this.model = {
+        this._model = {
             min: null,
             max: null,
             step: null,
@@ -25,82 +25,82 @@ class IncreaseBlock {
     }
 
     _createView(target) {
-        this.view.nodeClicked = $(target);
+        this._view.nodeClicked = $(target);
 
-        this.view.buttonBlock = this.view.nodeClicked
+        this._view.buttonBlock = this._view.nodeClicked
             .closest(".increase-form-element")
             .find(".increase-form-element__button-block");
 
-        this.view.increaseButton = this.view.buttonBlock.find(
+        this._view.increaseButton = this._view.buttonBlock.find(
             ".increase-form-element__button[data-button-type=inc]"
         );
-        this.view.decreaseButton = this.view.buttonBlock.find(
+        this._view.decreaseButton = this._view.buttonBlock.find(
             ".increase-form-element__button[data-button-type=dec]"
         );
-        this.view.valueLabel = this.view.buttonBlock.find(
+        this._view.valueLabel = this._view.buttonBlock.find(
             ".increase-form-element__value"
         );
     }
 
     _createModel(view) {
-        this.model.min = Number(view.buttonBlock.attr("data-min-value"));
-        this.model.max = Number(view.buttonBlock.attr("data-max-value"));
-        this.model.step = Number(view.buttonBlock.attr("data-step"));
-        this.model.currentValue = Number(view.valueLabel.html());
-        this.model.sign = view.nodeClicked.attr("data-button-type");
+        this._model.min = Number(view.buttonBlock.attr("data-min-value"));
+        this._model.max = Number(view.buttonBlock.attr("data-max-value"));
+        this._model.step = Number(view.buttonBlock.attr("data-step"));
+        this._model.currentValue = Number(view.valueLabel.html());
+        this._model.sign = view.nodeClicked.attr("data-button-type");
     }
 
     _modelChangeValue() {
-        const isDec = this.model.sign === CONSTANTS.DEC;
-        const lessOrEqualMin = this.model.min >= this.model.currentValue;
+        const isDec = this._model.sign === CONSTANTS.DEC;
+        const lessOrEqualMin = this._model.min >= this._model.currentValue;
         if (lessOrEqualMin && isDec) return;
 
-        const isInc = this.model.sign === CONSTANTS.INC;
-        const moreOrEqualMax = this.model.max <= this.model.currentValue;
+        const isInc = this._model.sign === CONSTANTS.INC;
+        const moreOrEqualMax = this._model.max <= this._model.currentValue;
         if (moreOrEqualMax && isInc) return;
 
-        this.model.currentValue = isDec
-            ? this.model.currentValue - this.model.step
-            : this.model.currentValue + this.model.step;
+        this._model.currentValue = isDec
+            ? this._model.currentValue - this._model.step
+            : this._model.currentValue + this._model.step;
     }
 
     _viewUpdate(current, min, max) {
-        this.view.valueLabel.html(current);
+        this._view.valueLabel.html(current);
 
         const currentLessMax = current < max;
-        this.view.increaseButton.prop("disabled", !currentLessMax);
+        this._view.increaseButton.prop("disabled", !currentLessMax);
 
         const currentMoreMin = current > min;
-        this.view.decreaseButton.prop("disabled", !currentMoreMin);
+        this._view.decreaseButton.prop("disabled", !currentMoreMin);
     }
 
     _nullifyModel() {
-        this.view.valueLabel.html(0);
+        this._view.valueLabel.html(0);
     }
     _nullifyView() {
-        this.view.valueLabel.html(0);
+        this._view.valueLabel.html(0);
     }
 
     onChange(target) {
         this._createView(target);
-        this._createModel(this.view);
+        this._createModel(this._view);
         this._modelChangeValue();
         this._viewUpdate(
-            this.model.currentValue,
-            this.model.min,
-            this.model.max
+            this._model.currentValue,
+            this._model.min,
+            this._model.max
         );
     }
 
     onReset(target) {
         this._createView(target);
-        this._createModel(this.view);
+        this._createModel(this._view);
         this._nullifyModel();
         this._nullifyView();
         this._viewUpdate(
-            this.model.currentValue,
-            this.model.min,
-            this.model.max
+            this._model.currentValue,
+            this._model.min,
+            this._model.max
         );
     }
 }
