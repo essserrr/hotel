9e1efb7ac8f181f467cf286c990314587bf6a231
Essserrr <blizzz93@gmail.com>
2021-01-30 12:05:57 +0900

    onBeforeWrite handler removed

--- src/ui-kit/masks/masks.js
+++ src/ui-kit/masks/masks.js
@@ -3,33 +3,11 @@ $(function () {
         console.log("Input mask is not connected");
         return;
     }
+
     $("[data-masktype=date]").inputmask({
         alias: "datetime",
         inputFormat: "dd.mm.yyyy",
         clearMaskOnLostFocus: false,
         placeholder: "ДД.ММ.ГГГГ",
-        onBeforeWrite: function (event, buffer, caretPos, opts) {
-            if (!event.target) return [event, buffer, caretPos, opts];
-
-            const defaultBuffer = opts.placeholder.split("");
-
-            let isDefaltValue = buffer.reduce(
-                (sum, value, key) => sum && defaultBuffer[key] === value,
-                true
-            );
-
-            if (
-                isDefaltValue &&
-                $(event.target).hasClass("ui-text-input--default-color")
-            ) {
-                $(event.target).removeClass("ui-text-input--default-color");
-            } else if (
-                !isDefaltValue &&
-                !$(event.target).hasClass("ui-text-input--default-color")
-            ) {
-                $(event.target).addClass("ui-text-input--default-color");
-            }
-            return [event, buffer, caretPos, opts];
-        },
     });
 });
